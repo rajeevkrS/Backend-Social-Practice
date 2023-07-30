64b87b570e55b5c45dc7b10f22983310d2a1cb62
Rajeev <ronaldocrcr5@gmail.com>
2023-07-30 19:30:01 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts'); //(npm install express-ejs-layouts)
 
 //use express layout
 app.use(expressLayouts);
